6af10737e9a87b6983de6255710d4207012cf07f
Vikas Pandey <vikashpandey340@gmail.com>
2022-10-23 12:02:20 +0530

    reverse-the-string

---
+++ easy/reverse-the-string_799927.cpp
@@ -0,0 +1,42 @@
+//https://www.codingninjas.com/codestudio/problems/reverse-the-string_799927
+#include <stdio.h>
+#include <stack>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+string reverseString(string str)
+{
+
+    string finalStr = "";
+    stack<char> st;
+    for (int i = 0; i < str.length(); i++)
+    {
+        st.push(str[i]);
+    }
+
+    while(!st.empty())
+    {
+        char cha = st.top();
+        finalStr.push_back(cha);
+         st.pop();
+    }
+
+    // Write your code here.
+
+    return finalStr;
+}
+
+int main()
+{
+
+    string s = "VikasPandey";
+    s = reverseString(s);
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        cout << s[i];
+    }
+
+    return 0;
+}
