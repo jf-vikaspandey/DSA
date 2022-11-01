83cecbeba621bcce4b46949b0ed97df2f2593c83
Vikas Pandey <vikas@code5.org>
2022-11-02 01:57:21 +0530

    make-the-string-great

---
+++ stack/easy/make-the-string-great.cpp
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include <stack>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+string makeGood(string s)
+{
+
+    stack<char> st;
+    st.push(s[0]);
+    for (int i = 1; i < s.length(); i++)
+    {
+        if (!st.empty() && (((st.top() == s[i] - 32)) || ((st.top() == s[i] + 32))))
+        {
+            st.pop();
+        }
+        else
+        {
+            st.push(s[i]);
+        }
+    }
+
+    string s1 = "";
+    while (!st.empty())
+    {
+
+        s1 = st.top() + s1;
+        st.pop();
+    }
+    cout << s1;
+    return s1;
+}
+
+int main()
+{
+
+    string s = "Ee";
+    s = makeGood(s);
+    return 0;
+}
