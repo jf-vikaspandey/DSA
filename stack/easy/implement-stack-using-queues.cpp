8eb40986dc5c02adf61688875f0618b813ef1a96
Vikas Pandey <vikas@code5.org>
2022-11-05 15:15:54 +0530

    implement-stack-using-queues

---
+++ stack/easy/implement-stack-using-queues.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <queue>
+#include <stdio.h>
+using namespace std;
+#include <iostream>
+#include <queue>
+class MyStack
+{
+public:
+    queue<int> q1;
+    queue<int> q2;
+    MyStack()
+    {
+    }
+
+    void push(int x)
+    {
+
+        q2.push(x);
+
+        for (int i = 0; i < q1.size(); i++)
+        {
+
+            q2.push(q1.front());
+            q1.pop();
+        }
+        queue<int> tmp = q1;
+        q1 = q2;
+        q2 = tmp;
+    }
+
+    int pop()
+    {
+        int a = q1.front();
+        ;
+
+        q1.pop();
+        return a;
+    }
+
+    int top()
+    {
+        return q1.front();
+    }
+
+    bool empty()
+    {
+        return q1.empty();
+    }
+};
+
+// Driver code
+int main()
+{
+    MyStack* obj = new MyStack();
+ obj->push(1);
+ obj->push(2);
+  
+  cout<< obj->top();
+    return 0;
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
